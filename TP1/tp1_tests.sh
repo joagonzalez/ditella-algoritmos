45cdc32439641a3307ab915c11d508867bd721a9
Joaquin Gonzalez <joagonzalez@gmail.com>
2020-04-14 20:31:04 -0300

    Update tp1_tests.sh

--- TP1/tp1_tests.sh
+++ TP1/tp1_tests.sh
@@ -8,34 +8,35 @@
 #                                                                                           |___/                                 
 # Agustin Alba Chicar
 # Joaquin Gonzalez
+# sh tp1_tests.sh <filename>
 #!/bin/bash
 echo "Comando a Ejecutar por línea de comando           |      Resultado impreso por pantalla"
 echo "----------------------------------------------------------------------------------"
-echo -n "python3 tp1.py esPrimo 2                          | " 
-python3 tp1.py esPrimo 2
-echo -n "python3 tp1.py esPrimo 6                          | " 
-python3 tp1.py esPrimo 6 
-echo -n "python3 tp1.py iesimoPrimo 1                      | " 
-python3 tp1.py iesimoPrimo 1
-echo -n "python3 tp1.py iesimoPrimo 5                      | " 
-python3 tp1.py iesimoPrimo 5  
-echo -n "python3 tp1.py cantidadPrimosMenoresOIguales 6    | " 
-python3 tp1.py cantidadPrimosMenoresOIguales 6
-echo -n "python3 tp1.py cantidadPrimosMenoresOIguales 7    | " 
-python3 tp1.py cantidadPrimosMenoresOIguales 7
-echo -n "python3 tp1.py cantidadDivisoresPrimos 6          | " 
-python3 tp1.py cantidadDivisoresPrimos 6
-echo -n "python3 tp1.py cantidadDivisoresPrimos 7          | " 
-python3 tp1.py cantidadDivisoresPrimos 7
-echo -n "python3 tp1.py iesimoDivisorPrimo 10 1            | " 
-python3 tp1.py iesimoDivisorPrimo 10 1
-echo -n "python3 tp1.py iesimoDivisorPrimo 10 2            | " 
-python3 tp1.py iesimoDivisorPrimo 10 2
-echo -n "python3 tp1.py iesimoDivisorPrimo 10 3            | " 
-python3 tp1.py iesimoDivisorPrimo 10 3
-echo -n "python3 tp1.py sumaPrimerosPrimos 1               | " 
-python3 tp1.py sumaPrimerosPrimos 1
-echo -n "python3 tp1.py sumaPrimerosPrimos 3               | " 
-python3 tp1.py sumaPrimerosPrimos 3
-echo -n "python3 tp1.py sumaPrimerosPrimos 5               | " 
-python3 tp1.py sumaPrimerosPrimos 5
+echo -n "python3 $1 esPrimo 2                          | " 
+python3 $1 esPrimo 2
+echo -n "python3 $1 esPrimo 6                          | " 
+python3 $1 esPrimo 6 
+echo -n "python3 $1 iesimoPrimo 1                      | " 
+python3 $1 iesimoPrimo 1
+echo -n "python3 $1 iesimoPrimo 5                      | " 
+python3 $1 iesimoPrimo 5  
+echo -n "python3 $1 cantidadPrimosMenoresOIguales 6    | " 
+python3 $1 cantidadPrimosMenoresOIguales 6
+echo -n "python3 $1 cantidadPrimosMenoresOIguales 7    | " 
+python3 $1 cantidadPrimosMenoresOIguales 7
+echo -n "python3 $1 cantidadDivisoresPrimos 6          | " 
+python3 $1 cantidadDivisoresPrimos 6
+echo -n "python3 $1 cantidadDivisoresPrimos 7          | " 
+python3 $1 cantidadDivisoresPrimos 7
+echo -n "python3 $1 iesimoDivisorPrimo 10 1            | " 
+python3 $1 iesimoDivisorPrimo 10 1
+echo -n "python3 $1 iesimoDivisorPrimo 10 2            | " 
+python3 $1 iesimoDivisorPrimo 10 2
+echo -n "python3 $1 iesimoDivisorPrimo 10 3            | " 
+python3 $1 iesimoDivisorPrimo 10 3
+echo -n "python3 $1 sumaPrimerosPrimos 1               | " 
+python3 $1 sumaPrimerosPrimos 1
+echo -n "python3 $1 sumaPrimerosPrimos 3               | " 
+python3 $1 sumaPrimerosPrimos 3
+echo -n "python3 $1 sumaPrimerosPrimos 5               | " 
+python3 $1 sumaPrimerosPrimos 5
