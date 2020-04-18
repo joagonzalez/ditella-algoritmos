5fa32d778cf5eb8642f5ad6cb8aba10be70d36c8
newcos <joagonzalez@gmail.com>
2020-04-18 22:36:14 +0000

    ej agenda resuelto

--- ejercicio_tipos/Agenda.py
+++ ejercicio_tipos/Agenda.py
@@ -3,20 +3,34 @@ class Agenda:
 
     # Crea una Agenda a partir de los contactos de "contactos.csv".
     def __init__(self):
+        self.agenda = []
         archivo = open("contactos.csv")
-        pass            # [reemplazar]
+        self.leer(archivo)        
         archivo.close()
 
+    def leer(self, datos):
+        for line in datos:
+            contacto = {}
+            line = line.split()
+            contacto['nombre'] = line[0].replace(',','')
+            contacto['mail'] = line[1].replace(',','')
+            self.agenda.append(contacto)
+        
     # Devuelve el mail del contacto 'nombre'.
     def mail(self, nombre):
-        return ""       # [reemplazar]
+        for contacto in self.agenda:
+            if contacto['nombre'] == nombre:
+                return contacto['mail']       
 
     # Devuelve el string que representa una agenda, mostrando un
     # contacto por línea. Formato de cada línea: [nombre]: [email]
     # Se tiene que remplazar el cuerpo de la función para que
     # devuelva el string esperado.
     def __str__(self):
-        return ""       # [reemplazar]
+        result = ''
+        for contacto in self.agenda:
+            result += '{}: {}\n'.format(contacto['nombre'], contacto['mail'])
+        return result
 
 # Cuerpo principal del programa
 agenda = Agenda()
@@ -25,3 +39,5 @@ for contacto in ["Alicia", "Juan"]:
 
 print()
 print(agenda)
+
+print(agenda.mail('Martina'))
