34691df3444d8ab814807e995a845feba687efb8
Joaquin Gonzalez <joagonzalez@gmail.com>
2020-04-25 19:09:49 -0300

    TP2

---
+++ TP2/src/dependencia_judicial.py
@@ -0,0 +1,13 @@
+class DependenciaJudicial():
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __eq__(self):
+        pass
+
+    def __le__(self):
+        pass
